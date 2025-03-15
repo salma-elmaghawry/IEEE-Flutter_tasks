0c7197d49f1fcefc8b204a35c905b4e7bbef762f
Salma Elmaghawry <salmaelmaghawry91@gmail.com>
2025-03-15 03:26:12 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_application_1/meals_app.dart';
+
+void main() {
+  runApp(const MealsApp());
+}
---
+++ lib/meals_app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Features/onboarding/onboarding_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MealsApp extends StatelessWidget {
+  const MealsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFFffffff),
+      
+        textTheme: GoogleFonts.ralewayTextTheme(Theme.of(context).textTheme),
+      ),
+      home: OnboardingScreen(),
+    );
+  }
+}
